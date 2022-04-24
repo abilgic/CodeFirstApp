508c5d2c9c72bf7297a94735dbc6ed2d6e82c9cb
ahmet bilgic <ahmet_bilgic@yahoo.com>
2022-04-24 11:07:49 +0300

    Add project files.

---
+++ CodeFirstApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstApp.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        DbSet<Job> Job { get; set; }
+    }
+}
